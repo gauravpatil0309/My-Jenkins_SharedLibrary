c3d2cab3e799bcc763aba24311d2b13cf46fa61c
gauravpatil0309 <96517580+gauravpatil0309@users.noreply.github.com>
2024-02-21 14:17:28 +0530

    Update installGradleJava.groovy
    new

--- vars/installGradleJava.groovy
+++ vars/installGradleJava.groovy
@@ -1,17 +1,20 @@
 def call() {
   node {
     stage('Install Gradle') {
+      // Choose a different directory where the Jenkins user has write permissions
+      def gradleDir = "${workspace}/gradle"
+
       // Install Gradle
-      sh 'wget https://services.gradle.org/distributions/gradle-7.6.4-bin.zip'
-      sh 'sudo su'
-      sh 'unzip -d /opt/gradle gradle-7.6.4-bin.zip'
-      sh 'export PATH=$PATH:/opt/gradle/gradle-7.6.4/bin'
+      sh "mkdir -p ${gradleDir}"
+      sh "wget https://services.gradle.org/distributions/gradle-7.6.4-bin.zip -P ${gradleDir}"
+      sh "unzip -d ${gradleDir} ${gradleDir}/gradle-7.6.4-bin.zip"
+      sh "export PATH=$PATH:${gradleDir}/gradle-7.6.4/bin"
     }
 
     stage('Install Java') {
       // Install Java
-      // sh 'yum install -y java-1.8.0'
-         sh 'amazon-linux-extras install java-openjdk11 -y'
+      //sh 'yum install -y java-1.8.0'
+      sh 'amazon-linux-extras install java-openjdk11 -y'
     }
   }
 }
