113d2bc9dabe768ab4f4202d133c5f57677291b9
gauravpatil0309 <96517580+gauravpatil0309@users.noreply.github.com>
2024-02-21 17:27:30 +0530

    Update installGradleJava.groovy

--- vars/installGradleJava.groovy
+++ vars/installGradleJava.groovy
@@ -2,17 +2,17 @@ def call() {
   node {
     stage('Check if gradle is already Installed') {
       // Choose a different directory where the Jenkins user has write permissions
-     // def gradleDir = "/root/gradle"
+      def gradleDir = "/root/gradle"
       def isGradleInstalled = sh(returnStdout: true, script: 'which gradle')
 
       if (isGradleInstalled == 0) {
         echo "Gradle is already installed"
       } else {
-      //sh "mkdir -p ${gradleDir}"
-      sh "wget https://services.gradle.org/distributions/gradle-7.6.4-bin.zip /tmp"
-      sh "unzip /tmp/gradle-7.6.4-bin.zip"
+      sh "mkdir -p ${gradleDir}"
+      sh "wget https://services.gradle.org/distributions/gradle-7.6.4-bin.zip -d ${gradleDir}"
+      sh "unzip -d ${gradleDir} ${gradleDir}/gradle-7.6.4-bin.zip"
       sh "which gradle"
-      sh "cp /tmp/gradle-7.6.4/bin/gradle /usr/local/bin"
+      sh "cp ${gradleDir}/gradle-7.6.4/bin/gradle /usr/local/bin"
       //sh "export PATH=$PATH:${gradleDir}/gradle-7.6.4/bin"
         }
       }    
