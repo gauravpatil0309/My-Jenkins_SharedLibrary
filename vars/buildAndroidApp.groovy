35d37d901915dce5efdbee577885e37136b9dca6
gauravpatil0309 <96517580+gauravpatil0309@users.noreply.github.com>
2024-03-05 17:15:19 +0530

    Update buildAndroidApp.groovy

--- vars/buildAndroidApp.groovy
+++ vars/buildAndroidApp.groovy
@@ -1,5 +1,5 @@
 def call() {
   //sh '/var/lib/jenkins/workspace/android-app/./gradlew clean'
   //sh '/var/lib/jenkins/workspace/android-app/./gradlew assembleDebug --info'
-    sh '/var/lib/jenkins/workspace/android-app/./gradlew build --stacktrace --debug'
+    sh '/var/lib/jenkins/workspace/android-app/./gradlew build --scan'
 }
