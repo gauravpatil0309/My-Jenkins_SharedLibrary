dd2f3f4d19e86fd80dfc31ad792787d6c3514022
gauravpatil0309 <96517580+gauravpatil0309@users.noreply.github.com>
2024-03-05 19:17:06 +0530

    Update buildAndroidApp.groovy

--- vars/buildAndroidApp.groovy
+++ vars/buildAndroidApp.groovy
@@ -1,5 +1,5 @@
 def call() {
   //sh '/var/lib/jenkins/workspace/android-app/./gradlew clean'
   //sh '/var/lib/jenkins/workspace/android-app/./gradlew assembleDebug --info'
-    sh 'sudo -H -u ec2-user bash -c "whoami && cd /var/lib/jenkins/workspace/android-app && ./gradlew assembleDebug --stacktrace"'
+    sh 'sudo -H -u ec2-user bash -c "whoami && cd /var/lib/jenkins/workspace/android-app && sudo ./gradlew assembleDebug"'
 }
