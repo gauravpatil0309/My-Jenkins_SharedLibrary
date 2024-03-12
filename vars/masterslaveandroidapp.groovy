b0a2e1f5545d07c556bb222a3548eb1a64979b62
Gaurav Patil <gaurav.patil02@globant.com>
2024-03-12 13:34:08 +0530

    new

---
+++ vars/masterslaveandroidapp.groovy
@@ -0,0 +1,5 @@
+def call() {
+  //sh '/var/lib/jenkins/workspace/android-app/./gradlew clean'
+  //sh '/var/lib/jenkins/workspace/android-app/./gradlew assembleDebug --info'
+    sh 'sudo -u ec2-user bash -c "whoami && cd /opt/build/workspace/master-slave-app-test && sudo ./gradlew assembleDebug"'
+}
