b09ab53c50dd3119c8439240cc15269447677041
gauravpatil0309 <96517580+gauravpatil0309@users.noreply.github.com>
2024-03-12 14:53:06 +0530

    Update masterslaveandroidapp.groovy

--- vars/masterslaveandroidapp.groovy
+++ vars/masterslaveandroidapp.groovy
@@ -1,5 +1,6 @@
 def call() {
   //sh '/var/lib/jenkins/workspace/android-app/./gradlew clean'
   //sh '/var/lib/jenkins/workspace/android-app/./gradlew assembleDebug --info'
+    sh 'chmod 777 -R /opt/build/workspace/master-slave-app-test/*'
     sh 'sudo -u ec2-user bash -c "whoami && cd /opt/build/workspace/master-slave-app-test && sudo ./gradlew assembleDebug"'
 }
